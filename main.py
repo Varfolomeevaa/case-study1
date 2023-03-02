0c783f9d1a226f41c43be0cb4660f9c5239c6355
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-03-02 11:14:39 +0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,40 @@
+'''
+Title 
+Group:
+
+
+'''
+
+def triangle(x,y,a,b,angle,color):
+    '''
+    Function for drawing triangle.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    :return: None
+    '''
+    pass
+
+def draw_dog():
+    '''
+
+    :return:
+    '''
+    pass
+
+def draw_cat():
+    pass
+
+def main():
+    '''
+    Main function.
+    :return: None
+    '''
+    draw_cat()
+    draw_dog()
+
+if __name__ == '__main':
+    main()
