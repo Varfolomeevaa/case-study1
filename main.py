516bbef67e230456887a8da2137186ac772799b6
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-03-09 11:18:54 +0700

    import

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ Group:
 Sineokya Anastasia
 Varfolomeeva Victoria
 '''
-
+import turtle
+import math
 def triangle(x,y,a,b,angle,color, border,border_color):
     '''
     Function for drawing triangle.
@@ -15,7 +16,7 @@ def triangle(x,y,a,b,angle,color, border,border_color):
     :param angle: angle of rotation
     :param color: color of triangle
     :param border: breadth of limit
-    :param border-color: color of the border
+    :param border_color: color of the border
     :return: None
     '''
     pass
@@ -32,6 +33,26 @@ def rectangle(x,y,a,b,color,border,border_color):
     :param border_color: color of the border
     :return: none
     '''
+    pass
+def ellipse(a, b, color='black', fill=''):
+    '''
+    Function for drawing ellipse
+    :param a:
+    :param b:
+    :param color:
+    :param fill:
+    :return:
+    '''
+    dx = turtle.xcor()
+    dy = turtle.ycor()
+    turtle.color(color, fill)
+    turtle.begin_fill()
+    for deg in range(361):
+        rad = math.radians(deg)
+        x = a * math.sin(rad) + dx
+        y = -b * math.cos(rad) + b + dy
+        turtle.goto(x, y)
+    turtle.end_fill()
 def main():
     '''
     Main function.
